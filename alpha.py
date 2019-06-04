a5c64857c894517dd5b0f30f5d6a74836bd969d8
Jeremiah Lübke <jer.luebke@web.de>
2019-06-04 08:46:26 +0200

    summarized arrays V, W into a single array; added documentation

--- alpha.py
+++ alpha.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
-"""Aliev-Panfilov model"""
+"""Aliev-Panfilov model: a phenomenological model of a myocytes action
+potential depending on two variables (given by two coupled ODEs) and five
+parameters.
+
+Compared to Hodgkin-Huxley (or similar), this model is much less complex as all
+the biophysical details of ion pump dynamics are summarized in a single
+phenomenological relaxation variable W. Thus it is more suitable for large
+scale simulations of tissue.
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 class Alpha:
+    """Aliev-Panfilov simulation class"""
+
     def __init__(self, V0, W0, tmax, dt, a, k, e0, m1, m2):
         # set simulation parameters
         self.__dict__.update(dict(a=a, k=k, e0=e0, m1=m1, m2=m2))
@@ -13,11 +23,10 @@ class Alpha:
         self.dt     = dt
         self.steps  = self.t.size
 
-        # result arrays
-        self.V      = np.zeros(self.steps)
-        self.W      = np.zeros(self.steps)
-        self.V[0]   = V0
-        self.W[0]   = W0
+        # result array
+        self.VW     = np.zeros((2, self.steps))
+        self.VW[0,0]= V0
+        self.VW[1,0]= W0
 
         # plotting
         self.fig        = plt.figure()
@@ -30,10 +39,13 @@ class Alpha:
 
 
     def G(self, V, W):
-        return (
+        """equations for V, w:
+            [dV/dt, dW/dt] = G(V, W)
+        """
+        return np.array([
             -self.k * V * (V - self.a) * (V - 1) - V * W,
             self.e(V, W) * (-self.k * V * (V - self.a - 1) - W)
-        )
+        ])
 
 
     def e(self, V, W):
@@ -41,40 +53,57 @@ class Alpha:
 
 
     def integrate(self):
+        """integrate the ODE system with a simple Euler-step
+        """
         for i in range(self.steps-1):
-            V_i, W_i    = self.V[i], self.W[i]
-            V_dot, W_dot= self.G(V_i, W_i)
-            self.V[i+1] = V_i + self.dt * V_dot
-            self.W[i+1] = W_i + self.dt * W_dot
+            VW_i = self.VW[:,i]
+            self.VW[:,i+1] = VW_i + self.dt * self.G(*VW_i)
 
 
     def portrait(self):
+        """create a quiver plot of [dV/dt, dW/dt] to visualize the phase space
+        dynamics of the ODEs
+        """
         V_mesh, W_mesh = np.mgrid[0:1.1:.1,0:2.5:.2]
         V_dot, W_dot = self.G(V_mesh, W_mesh)
         self.phaseplot.quiver(V_mesh, W_mesh, V_dot, W_dot,
                               color='grey', scale=15, width=.004)
 
 
-    def plot(self, legend_entry=None):
+    def plot(self):
+        """integrate the ODEs for the paarameters of the instance and plot the
+        results, showing the development of action potential V and relaxation W
+        over time.
+
+        the obtained results need to be scaled in order to correspond to the
+        'true' physical quantities:
+            V_phys = (100*V-80) mV
+            t_phys = 12.9*t ms
+        """
         self.integrate()
-        self.phaseplot.plot(self.V, self.W)
-        self.timeplot.plot(12.9*self.t, 100*self.V-80,
-                           12.9*self.t, 100*self.W-80)
+        self.phaseplot.plot(self.VW[0], self.VW[1])
+        self.timeplot.plot(12.9*self.t, 100*self.VW[0]-80,
+                           12.9*self.t, 100*self.VW[1]-80)
         self.timeplot.legend(['action potential V', 'relaxation variable W'],
                              loc='upper right')
 
 
     def vary_param(self, key, val_list):
+        """given the name of a parameter of the simulation and a list of its
+        possible values, integrate the system for each value and plot the
+        results, in order to get an understanding of how the given parameter
+        influences the dynamics of the system
+        """
         colors = plt.get_cmap('Dark2').colors
         for val, color in zip(val_list, colors):
             setattr(self, key, val)
             self.integrate()
-            self.phaseplot.plot(self.V, self.W,
+            self.phaseplot.plot(self.VW[0], self.VW[1],
                                 color=color,
                                 label='%s=%s' % (key, val))
-            self.timeplot.plot(12.9*self.t, 100*self.V-80,
+            self.timeplot.plot(12.9*self.t, 100*self.VW[0]-80,
                                color=color)
-            self.timeplot.plot(12.9*self.t, 100*self.W-80,
+            self.timeplot.plot(12.9*self.t, 100*self.VW[1]-80,
                                color=color,
                                label='%s=%s' % (key, val))
         self.phaseplot.legend(loc='upper right')
@@ -82,24 +111,28 @@ class Alpha:
 
 
 
-params  = dict(a=.15, k=8., e0=2e-3, m1=.2, m2=.3)
-alpha   = Alpha(.2, .0, 60, .01, **params)
+params = dict(
+    V0=.2, W0=.0, tmax=60., dt=.01,
+    a=.15, k=8., e0=2e-3, m1=.2, m2=.3
+)
+
+alpha = Alpha(**params)
 alpha.portrait()
 alpha.plot()
 
-alpha   = Alpha(.2, .0, 60, .01, **params)
+alpha = Alpha(**params)
 alpha.vary_param('a', [.1, .125, .15, .175, .199, .2])
 
-alpha   = Alpha(.2, .0, 60, .01, **params)
+alpha = Alpha(**params)
 alpha.vary_param('k', [4., 6., 8., 10., 12., 14.])
 
-alpha   = Alpha(.2, .0, 60, .01, **params)
+alpha = Alpha(**params)
 alpha.vary_param('e0', [1e-5, 1e-4, 2e-3, 1e-1, 1.])
 
-alpha   = Alpha(.2, .0, 60, .01, **params)
+alpha = Alpha(**params)
 alpha.vary_param('m1', [.05, .1, .2, .5, 1., 2.])
 
-alpha   = Alpha(.2, .0, 60, .01, **params)
+alpha = Alpha(**params)
 alpha.vary_param('m2', [.1, .3, 1., 2., 5.])
 
 

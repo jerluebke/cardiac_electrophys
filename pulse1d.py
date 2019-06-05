82ce3ecedd09c9783d777660e2ad9a59a10726aa
Jeremiah Lübke <jer.luebke@web.de>
2019-06-05 15:35:53 +0200

    added measurement of CV, BCL, APD

--- pulse1d.py
+++ pulse1d.py
@@ -22,8 +22,9 @@ class Pulse1d(AlphaBase):
         self.dx     = dx
         self.eta    = eta
         self.dt     = .1 * dx**2 / (2. * eta)
+        self.t      = np.arange(0, tmax, self.dt)
         self.x      = np.arange(0, xmax, dx)
-        self.steps  = int(np.ceil(tmax / self.dt))
+        self.steps  = self.t.size
         self.V      = np.zeros_like(self.x)
         self.W      = np.zeros_like(self.x)
         self.LV     = np.zeros_like(self.x)
@@ -31,33 +32,66 @@ class Pulse1d(AlphaBase):
 
 
     def integrate(self):
+        # pulling instance variables to local scope
         V, W, LV = self.V, self.W, self.LV
-        dx, dt, eta = self.dx, self.dt, self.eta
+        xmax, t, dx, dt, eta = self.x[-1], self.t, self.dx, self.dt, self.eta
+
+        # initial excitation of action potential
         V[0] = 1.
+        # initial boundary condition
         bc = 'neumann'
 
+        # variables for measuring CV, BCL, APD
+        threshold   = .1
+        previous_V  = 0.
+        uptime      = -10.
+
         for i in range(self.steps-1):
+            # compute laplacian and alpha step
             Lap1d(V, LV, dx, bc)
             dV, dW = self.G(V, W)
+
+            # euler step
             V += dt * dV + dt * eta * LV
             W += dt * dW
 
+            # current measurement
+            now         = t[i]
+            current_V   = V[10]
+
+            if current_V > threshold and previous_V < threshold:
+                # detected upstroke
+                CV      = xmax / (now - uptime)
+                BCL     = now - uptime
+                uptime  = now
+                print('UPSTROKE:\n\tCV = %e\n\tBCL = %e\n' % (CV, BCL))
+
+            if current_V < threshold and previous_V > threshold:
+                # detected downstroke
+                APD = now - uptime
+                print('DOWNSTROKE:\n\tAPD = %e\n' % APD)
+
+            previous_V = current_V
+
+            # switching boundary condition to periodic, after enough time for
+            # the pulse to develop
             if i == 4_000:
                 bc = 'periodic'
+                print('switche boundary condition to periodic...\n')
 
             if i % self.plot_interval == 0:
                 yield V
 
 
 
+params = dict(a=.15, k=8., e0=2e-3, m1=.2, m2=.3)
+p = Pulse1d(100, .2, 1000., .3, 50, **params)
+pulse_gen = p.integrate()
+
 fig, ax = plt.subplots()
 ax.set(title='action potential propagation', xlabel='x', ylabel='V',
-       xlim=(0, 200), ylim=(-.1, 1.1))
+       xlim=(0, p.x[-1]), ylim=(-.1, 1.1))
 V_line, = ax.plot([], [])
-
-params = dict(a=.15, k=8., e0=2e-3, m1=.2, m2=.3)
-p = Pulse1d(200, .2, 5000., .3, 50, **params)
-pulse_gen = p.integrate()
 
 
 def init():

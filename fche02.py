349740bba109c8ee31937010cb4327e4ec6e9ab5
Jeremiah Lübke <jer.luebke@web.de>
2019-06-19 00:38:45 +0200

    restructured fche02

--- fche02.py
+++ fche02.py
@@ -18,69 +18,105 @@ def Lap2d(a, o, dx):
 
 
 
-class FCHE:
-
-    def __init__(self, xmax, ymax, dx, tmax, eta, c,
-                 Vc, Vc_si, Vv, k, t0, td, tr, tsi, tv1, tv2, tvp, twm, twp,
-                 plot_interval=50):
+class FCHE_Base:
+    def __init__(self, Vc, Vc_si, Vv, k, c,
+                 t0, td, tr, tsi, tv1, tv2, tvp, twm, twp):
         # set simulation params
         self.__dict__.update(dict(
-            Vc=Vc, Vc_si=Vc_si, Vv=Vv, k=k, t0=t0, td=td, tr=tr, tsi=tsi,
+            Vc=Vc, Vc_si=Vc_si, Vv=Vv, k=k, c=c, t0=t0, td=td, tr=tr, tsi=tsi,
             tv1=tv1, tv2=tv2, tvp=tvp, twm=twm, twp=twp
         ))
 
-        self.c      = c     # specific capacity
-        self.dx     = dx    # lattice constant
-        self.eta    = eta   # diffusity
+    def _I_fi(self, V, v, p):
+        return -v * p * (V - self.Vc) * (1 - V) / self.td
+
+    def _I_so(self, V, p):
+        return V * (1 - p) / self.t0 + p / self.tr
+
+    def _I_si(self, V, w):
+        return -w * (1 + np.tanh(self.k * (V - self.Vc_si))) / (2 * self.tsi)
+
+    def Gv(self, v, p, q):
+        return (1 - p) * (1 - v) / ((1 - q) * self.tv1 + q * self.tv2) \
+                - p * v / self.tvp
+
+    def Gw(self, w, p):
+        return (1 - p) * (1 - w) / self.twm - p * w / self.twp
+
+    def GV(self, V, v, w, p):
+        return -(self._I_fi(V, v) + self._I_so(V, p) + self._I_si(V, w)) / self.c
+
+
+
+class FCHE_Single_Cell(FCHE_Base):
+    def __init__(self, V0, v0, w0, tmax, dt, **params):
+        super().__init__(**params)
+        self.t      = np.arange(0, tmax, dt)
+        self.dt     = dt
+        self.steps  = self.t.size
+
+        self.V = np.zeros_like(self.t)
+        self.v = np.zeros_like(self.t)
+        self.w = np.zeros_like(self.t)
+
+
+    def integrate(self):
+        V, v, w = self.V, self.v, self.w
+        p = np.ones_like(V)
+        q = np.ones_like(V)
+
+        for i in range(self.steps-1):
+            p[:] = q[:] = 1.
+            p[V < self.Vc] = 0.
+            q[V < self.Vv] = 0.
+
+            V += self.dt * self.GV(V, v, w, p)
+            v += self.dt * self.Gv(v, p, q)
+            w += self.dt * self.Gw(w, p)
+
+
+
+class FCHE_2D(FCHE_Base):
+
+    def __init__(self, xmax, ymax, dx, tmax, eta,
+                 plot_interval=50, **params):
+        super().__init__(**params)
+
+        self.dx     = dx                        # lattice constant
+        self.eta    = eta                       # diffusity
         self.dt     = .2 * dx**2 / (2. * eta)   # cfl condition
         self.steps  = tmax // self.dt + 1.
 
         self.X, self.Y = np.mgrid[0:xmax:dx,0:ymax:dx]
-        self.V = np.zeros_like(self.X)
+        self.V  = np.zeros_like(self.X)
+        self.v  = np.zeros_like(self.X)
+        self.w  = np.zeros_like(self.X)
+        self.LV = np.zeros_like(self.X)
 
         self.plot_interval = plot_interval
 
 
-    def G(self, V, v, w, p, q, out):
-        # pull params to local scope
-        I_fi, I_so, I_si    = self.I_fi, self.I_so, self.I_si
-        Vc, Vc_si, k, c     = self.Vc, self.Vc_si, self.k, self.c
-        t0, td, tr, tsi     = self.t0, self.td, self.tr, self.tsi
-
-        # compute currents
-        I_fi[:] =  -v * p * (V-Vc) * (1-V) / td
-        I_so[:] = V * (1-p) / t0 + p / tr
-        I_si[:] = -w * (1 + np.tanh(k * (V-Vc_si))) / (2 * tsi)
-        out[:]  = -(I_fi + I_so + I_si) / c
-
-        return out
-
-
     def integrate(self):
-        # pull params to local scope
-        Vc, Vv, dx, dt, eta     = self.Vc, self.Vv, self.dx, self.dt, self.eta
-        tv1, tv2, tvp, twm, twp =  self.tv1, self.tv2, self.tvp, self.twm, self.twp
+        # pull params and arrays to local scope
+        Vc, Vv, dx, dt, eta = self.Vc, self.Vv, self.dx, self.dt, self.eta
+        V, v, w, LV = self.V, self.v, self.w. self.LV
 
-        # init arrays
-        V   = self.V
-        dV  = np.zeros_like(V)
-        LV  = np.zeros_like(V)
-        v   = np.zeros_like(V)
-        w   = np.zeros_like(V)
-        p   = np.ones_like(V)
-        q   = np.ones_like(V)
+        p = np.ones_like(V)
+        q = np.ones_like(V)
 
         for i in range(self.steps-1):
             p[:] = q[:] = 1.
             p[V < Vc] = 0.
             q[V < Vv] = 0.
 
-            LV = Lap2d(V, dx, out=LV, both='neumann')
-            dV = self.G(V, out=dV)
-            V += dt * eta * LV + dt * dV
-            v += dt * (1-p) * (1-v) / ((1-q) * tv1 + q * tv2) - dt * p * v / tvp
-            w += dt * (1-p) * (1-w) / twm - dt * p * w / twp
+            Lap2d(V, dx, out=LV, both='neumann')
+            V += dt * eta * LV + dt * self.GV(V, v, w, p)
+            v += dt * self.Gv(v, p, q)
+            w += dt * self.Gw(w, p)
+
+            if i % self.plot_interval == 0:
+                yield V
 
 
 
-#  vim: set ff=unix tw=79 sw=4 ts=8 et ic ai : 
+#  vim: set ff=unix tw=79 sw=4 ts=8 et ic ai :
